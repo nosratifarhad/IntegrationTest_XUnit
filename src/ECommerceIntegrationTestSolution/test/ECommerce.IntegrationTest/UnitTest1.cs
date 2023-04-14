af96996f0ea4abf41dbb1f35334249eae7e7bb04
f_nosrati <f_nosrati@SHL-62>
2023-04-14 12:17:38 +0330

    Add ProductsControllerIntegrationTests

--- src/ECommerceIntegrationTestSolution/test/ECommerce.IntegrationTest/UnitTest1.cs
+++ src/ECommerceIntegrationTestSolution/test/ECommerce.IntegrationTest/UnitTest1.cs
@@ -1,11 +1,32 @@
+using ECommerce.Api;
+using Microsoft.AspNetCore.Mvc.Testing;
+
 namespace ECommerce.IntegrationTest
 {
-    public class UnitTest1
+    public class ProductsControllerIntegrationTests : IClassFixture<WebApplicationFactory<Startup>>
     {
-        [Fact]
-        public void Test1()
-        {
+        #region Fields
+        private readonly HttpClient _client;
 
+        #endregion Fields
+
+        #region Ctor
+        public ProductsControllerIntegrationTests(WebApplicationFactory<Startup> factory)
+        {
+            _client = factory.CreateClient();
+        }
+        #endregion Ctor
+
+        [Fact]
+        public async Task Test1()
+        {
+            var response = await _client.GetAsync("/api/products");
+
+            //var responseString = await response.Content.ReadAsStringAsync();
+
+            //// Assert
+            //Assert.Equal("Hello World!",
+            //    responseString);
         }
     }
 }
