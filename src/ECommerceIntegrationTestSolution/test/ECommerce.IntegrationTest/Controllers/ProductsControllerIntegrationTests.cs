1a48234454a1b4dfbaf4ee4a08bbd1f60e1589fa
f_nosrati <f_nosrati@SHL-62>
2023-04-14 15:35:45 +0330

    Add Integration test for create product

--- src/ECommerceIntegrationTestSolution/test/ECommerce.IntegrationTest/Controllers/ProductsControllerIntegrationTests.cs
+++ src/ECommerceIntegrationTestSolution/test/ECommerce.IntegrationTest/Controllers/ProductsControllerIntegrationTests.cs
@@ -1,8 +1,15 @@
+using Bogus;
 using ECommerce.Api;
 using ECommerce.IntegrationTest.Extensions;
+using ECommerce.IntegrationTest.MockDatas;
+using ECommerce.Service.InputModels.ProductInputModels;
 using ECommerce.Service.ViewModels.ProductViewModels;
 using FluentAssertions;
 using Microsoft.AspNetCore.Mvc.Testing;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Newtonsoft.Json;
+using System.Text;
+using Assert = Xunit.Assert;
 
 namespace ECommerce.IntegrationTest.Controllers
 {
@@ -21,18 +28,98 @@ namespace ECommerce.IntegrationTest.Controllers
         #endregion Ctor
 
         #region [ GetProducts ] 
-        
+
         [Fact]
         public async Task When_CallGetProducts_Then_ReturnedproductViewModelList()
         {
             var response = await _client.GetAsync("/api/products").ConfigureAwait(false);
 
-            var responseModel = await response.ResponseToViewModel<IEnumerable<ProductViewModel>>();
+            var responseModel = await response.ToResponseModel<IEnumerable<ProductViewModel>>();
 
             responseModel.Should().NotBeNull();
             responseModel.Should().HaveCount(5);
         }
 
         #endregion [ GetProducts ] 
+
+        #region [ CreateProduct ]
+
+        [Fact]
+        public async Task When_ValidCreateProductInputModelInCreateProduct_Then_CreatestProductInDataBase()
+        {
+            var mockInputModel = ProductMockData.ValidCreateProductInputModel().ToRequestModel();
+
+            var response = await _client.PostAsync("/api/product", mockInputModel).ConfigureAwait(false);
+
+            response.StatusCode.Should().Be(System.Net.HttpStatusCode.Created);
+            response.IsSuccessStatusCode.Should().BeTrue();
+
+            var productId = await response.ToRequestItem("ProductId");
+
+            var newProductViewModel = await GetProductByResponseHeadersLocation(response.Headers.Location.AbsoluteUri).ConfigureAwait(false);
+
+            newProductViewModel.Should().NotBeNull();
+            newProductViewModel.ProductId.Should().Be(productId);
+        }
+
+
+        [Fact]
+        [ExpectedException(typeof(ArgumentException),"Product Name cannot be nul")]
+        public async Task When_ProductNameIsNullInC1reateProductInputModelInCreateProduct_Then_ProductNameCannotBeNullThrowException()
+        {
+            var mockInputModel = ProductMockData.ProductNameIsNullInC1reateProductInputModel().ToRequestModel();
+
+            Action action = () => _client.PostAsync("/api/product", mockInputModel).GetAwaiter().GetResult();
+
+            action.Should().Throw<ArgumentException>();
+
+            //Assert.ThrowsAsync<ArgumentException>(async () => await _client.PostAsync("/api/product", mockInputModel));
+
+            //_client.Invoking(a => a.PostAsync("/api/product", mockInputModel).GetAwaiter().GetResult())
+            //    .Should().Throw<ArgumentException>().WithMessage("Product Name cannot be null");
+
+            //Action action = async () => await _client.PostAsync("/api/product", mockInputModel);
+            //action.Should().Throw<ArgumentException>();
+
+            // Asserts that calling GetId() will throw
+            //FluentActions.Invoking(() => _client.PostAsync("/api/product", mockInputModel)).Should().ThrowAsync<NullReferenceException>();
+
+            //Func<Task> act = async () => await _client.PostAsync("/api/product", mockInputModel);
+            //act.Should().ThrowAsync<NullReferenceException>();
+
+            //Action action = async () => await _client.PostAsync("/api/product", mockInputModel);
+            //action.Should().Throw<NullReferenceException>().Where(ex => ex.Message != null).Where(ex => ex.Data.Count > 0);
+        }
+
+        [Fact]
+        [ExpectedException(typeof(ArgumentException), "Product Title cannot be null.")]
+        public async Task When_ProductTitleIsNullInCreateProductInputModelInCreateProduct_Then_ProductTitleCannotBeNullThrowException()
+        {
+            var mockInputModel = ProductMockData.ProductTitleIsNullInCreateProductInputModel().ToRequestModel();
+
+            Func<Task> f = async () => { await _client.PostAsync("/api/product", mockInputModel).ConfigureAwait(false); };
+
+            f.Should()?.ThrowAsync<ArgumentException>()
+                .WithParameterName("Product Title cannot be null.");
+
+        }
+
+
+        #endregion [ CreateProduct ]
+
+
+        #region [ Private ]
+
+        private async Task<ProductViewModel> GetProductByResponseHeadersLocation(string location)
+        {
+            var responseProductViewModel = await _client.GetAsync(location).ConfigureAwait(false);
+
+            var newProductViewModel = await responseProductViewModel.ToResponseModel<ProductViewModel>();
+
+            return newProductViewModel;
+        }
+
+        #endregion [ Private ]
+
     }
 }
--- src/ECommerceIntegrationTestSolution/test/ECommerce.IntegrationTest/Extensions/JsonConvertExtension.cs
+++ src/ECommerceIntegrationTestSolution/test/ECommerce.IntegrationTest/Extensions/JsonConvertExtension.cs
@@ -1,10 +1,53 @@
 ﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
 
 namespace ECommerce.IntegrationTest.Extensions
 {
     public static class JsonConvertExtension
     {
-        public static async Task<T> ResponseToViewModel<T>(this HttpResponseMessage response)
+        public static async Task<T> ToResponseModel<T>(this HttpResponseMessage response)
             => JsonConvert.DeserializeObject<T>(await response.Content.ReadAsStringAsync());
+
+        public static StringContent ToRequestModel<T>(this T requestModel)
+            => new StringContent(JsonConvert.SerializeObject(requestModel), Encoding.UTF8, "application/json");
+
+        public static async Task<int> ToRequestItem(this HttpResponseMessage response, string itemName)
+        {
+            var camelCaseItemName = itemName.ToCamelCase();
+
+            var json = await response.Content.ReadAsStringAsync();
+
+            int businessItem = (int)json.ToJObject().SelectToken(camelCaseItemName);
+
+            return businessItem;
+        }
+
+        public static JObject ToJObject(this string jsonSerializer)
+             => JObject.Parse(jsonSerializer);
+
+        #region Private
+
+        private static string ToCamelCase(this string str)
+        {
+            string[] words = str.Split(new[] { "_", " " }, StringSplitOptions.RemoveEmptyEntries);
+
+            string leadWord = Regex.Replace(words[0], @"([A-Z])([A-Z]+|[a-z0-9]+)($|[A-Z]\w*)",
+                m =>
+                {
+                    return m.Groups[1].Value.ToLower() + m.Groups[2].Value.ToLower() + m.Groups[3].Value;
+                });
+
+            string[] tailWords = words.Skip(1)
+                .Select(word => char.ToUpper(word[0]) + word.Substring(1))
+                .ToArray();
+
+            var camelCaseStr = $"{leadWord}{string.Join(string.Empty, tailWords)}";
+
+            return camelCaseStr;
+        }
+
+        #endregion Private
     }
 }
